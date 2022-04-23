5f8acd75000c0fa8ad0f0fd1a19984838e3088a9
DamianMtnk <wrx71582@student.wsb.wroclaw.pl>
2022-04-23 08:59:04 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -16,3 +16,15 @@ $array[1] = 5;
 $array[2] = 3;
 echo core_mean($array = $array);
 ?>
+
+<?php
+$oceny = array(1,2,3,4,5);
+$wagi = array(5,4,3,2,1);
+$sumaWazona = 0;
+foreach( $oceny as $klucz => $ocena ){
+  $sumaWazona +=  $ocena * $wagi[$klucz];
+  $sumaWag += $wagi[$klucz];
+}
+ 
+$wynik = $sumaWazona / $sumaWag;
+?>
